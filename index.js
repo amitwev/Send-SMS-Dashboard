87505f717149d872300667df0cb8875caebae536
Amit Salim <amitwev@gmail.com>
2022-11-27 17:36:31 +0200

    fix port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const helpers = require('./helpers');
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001 ;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
